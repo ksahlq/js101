3d0806554b9d61733ffe91828f73bef270acd8b3
Khalid Sahlqeath <khalid.sahlqeath@outlook.com>
2020-12-11 17:25:07 +0100

    added alternative solution to welcome stranger

--- small-problems/easy2/welcome-stranger.js
+++ small-problems/easy2/welcome-stranger.js
@@ -5,4 +5,10 @@ function greetings(array, object) {
   return `Hello, ${name}! Nice to have a ${title} around.`;
 }
 
+// alternative solution
+let greetings_2  = (array, object) => {
+  return `Hello, ${array.join(' ')}! Nice to have a ${object.title} ${object.occupation} around.`;
+}
+
 console.log(greetings(["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" }));
+console.log(greetings_2(["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" }));
