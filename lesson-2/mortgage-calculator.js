778e2ac991553fe970e69140dad808954a9dc9f5
Khalid Sahlqeath <khalid.sahlqeath@outlook.com>
2020-12-27 18:44:00 +0100

    working on mortgage calculator

--- lesson-2/mortgage-calculator.js
+++ lesson-2/mortgage-calculator.js
@@ -23,6 +23,7 @@ Calculate:
   * loan duration in months
 */
 let readline = require('readline-sync');
+let messages = require('./mortgage-calculator-messages.json');
 
 function prompt(input) {
   console.log(`=> ${input}`);
@@ -33,28 +34,58 @@ function invalidNumber(number) {
 }
 
 function askLoanAmount() {
-  let loanAmount = parseFloat(readline.question('How much do you wish to loan?\n'));
+  prompt(messages.queryLoanAmount);
+  let loanAmount = parseFloat(readline.question());
 
   while (invalidNumber(loanAmount)) {
-    prompt('Ehm...that is not a valid loan number');
+    prompt(messages.invalidLoan);
     loanAmount = parseFloat(readline.question());
   }
   return loanAmount;
 }
 
 function askAnnualPercentageRate() {
-  let apr = parseFloat(readline.question('What is the Annual Percentage Rate?\n'));
+  prompt(messages.queryRate);
+  let apr = parseFloat(readline.question());
+
+  while (invalidNumber(apr)) {
+    prompt(messages.invalidRate);
+    apr = parseFloat(readline.question());
+  }
   return apr;
 }
 
 function askLoanDuration() {
-  let yearDuration = parseInt(readline.question('Type in loan duration in years?\n'), 10);
-  let monthDuration = parseInt(readline.question('Type in loan duration in months?\n'), 10);
+  prompt(messages.queryYears);
+  let yearDuration = parseInt(readline.question(), 10);
+
+  while (invalidNumber(yearDuration)) {
+    prompt(messages.invalidDuration);
+    yearDuration = parseInt(readline.question(), 10);
+  }
+  prompt(messages.queryMonths);
+  let monthDuration = parseInt(readline.question(), 10);
+
+  while (invalidNumber(monthDuration)) {
+    prompt(messages.invalidDuration);
+    monthDuration = parseInt(readline.question(), 10);
+  }
+
   let loanDuration = (yearDuration * 12) + monthDuration;
   return loanDuration;
 }
 
+function monthlyInterestRate(apr, duration) {
+  return apr / duration;
+}
 
-prompt(askLoanAmount());
-prompt(`${askAnnualPercentageRate()}%`);
-prompt(`The total loan duration in months is: ${askLoanDuration()}`);
+// TODO: perform this after all input has been done and
+function noInterestLoan() {
+  return askLoanAmount() / askLoanDuration();
+}
+askLoanAmount();
+/* console.log(`${askAnnualPercentageRate()}%`);
+console.log(`The total loan duration in months is: ${askLoanDuration()}`); */
+let apr = askAnnualPercentageRate();
+let loanDuration = askLoanDuration();
+console.log(`Monthly interest rate: ${monthlyInterestRate(apr, loanDuration)}`);
