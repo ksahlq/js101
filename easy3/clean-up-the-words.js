baae537b50e0bc37093e6a9429c7b1ac900495b1
Khalid Sahlqeath <khalid.sahlqeath@outlook.com>
2021-01-14 22:11:01 +0100

    easy 3 continues part 2...

--- easy3/clean-up-the-words.js
+++ easy3/clean-up-the-words.js
@@ -1,27 +1,3 @@
-/* 
-Understand the problem
-  - Input
-    - a string 
-  - Output
-    - return modified input string
-  - Rules
-    - 1. all non-alphabetic characters should be replaced with space
-      2. no consecutive spaces
-
-Algorithm
-  - iterate through the string, check if it's previous index is an alphabet or if there is a space
-    - if true, then return the alphabet
-  - else then make current index into a space
-
-  - iterate through the string, if the previous index is an alphabet, add space, if not remove element
-
-  - iteratate through the string
-    - if previous index is not alphabet AND current index is not alphabet, insert space
-    - else remove current index
-  
-
-*/
-
 function isLowerCaseLetter(char) {
   return char >= 'a' && char <= 'z';
 }
@@ -41,6 +17,11 @@ function cleanUp(string) {
     }
   }
   return cleanText;
+}
+
+// alternative solution
+function cleanUp(text) {
+  return text.replace(/[^a-z]/gi, " ").replace(/\s+/gi, " ");
 }
 
 console.log(cleanUp("---what's my +*& line?"));
