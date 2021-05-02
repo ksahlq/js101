5bf7ba8cdafdad018d14e0d78ac892be50863441
Khalid Sahlqeath <khalid.sahlqeath@outlook.com>
2021-05-02 19:54:37 +0200

    done with string and text processing

--- string-and-text-processing/staggered-caps-part2.js
+++ string-and-text-processing/staggered-caps-part2.js
@@ -8,8 +8,8 @@ function staggeredCase(string) {
   return chars.join('');
 }
 
+console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
+console.log(staggeredCase("ALL CAPS") === "AlL cApS");
 console.log(
-  staggeredCase('I Love Launch School!'),
-  staggeredCase('ALL_CAPS'),
-  staggeredCase('ignore 77 the 4444 numbers')
+  staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
 );
