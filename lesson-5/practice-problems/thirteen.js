9068921d94933dbabedc26fe3c912388e2005a2b
Khalid Sahlqeath <khalid.sahlqeath@outlook.com>
2021-05-15 20:43:14 +0200

    exercise thirteen done

--- lesson-5/practice-problems/thirteen.js
+++ lesson-5/practice-problems/thirteen.js
@@ -1,9 +1,14 @@
 let arr = [[1, 6, 7], [1, 5, 3], [1, 8, 3]];
 
 let oddSorted = arr.sort((a, b) => {
-  return a - b;
+  let oddSumA = a.filter(num => num % 2 === 1)
+    .reduce((acc, curr) => acc + curr);
+  let oddSumB = b.filter(num => num % 2 === 1)
+    .reduce((acc, curr) => acc + curr);
+
+  return oddSumA - oddSumB;
 });
 
 console.log(
-  oddSorted
+  oddSorted,
 );
